4c0340c36b1016ef0dbaf86d6a4d0cd5e0a84a1f
Alyaesub <dadz13104@gmail.com>
2025-04-25 11:08:49 -0700

    start js bb

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ const tableProject = document.getElementById("projectTableBody");
 
 //fonction qui ajoute un nouveau projet dans le tableau en dessous le l'input
 buttonAddProject.addEventListener("click", () => {
-	const projectName = inputNewProject.ariaValueMax.trim();
+	const projectName = inputNewProject.value.trim();
 	if (projectName === "") {
 		return;
 	}
+	const newRow = document.createElement("tr"); //constante qui me crée un nouvelle ligne dans le tableau
+	const nameCell = document.createElement("td"); //pareil mais pour une cellule ou il y aura le titre du projet
+	nameCell.textContent = projectName; // la cellul en question
+	newRow.appendChild(nameCell); //nouvelle ligne qui a le nom de la cellule
+	tableProject.appendChild(newRow); // et création de la ligne dans le tableau
+	inputNewProject.value = ""; //rest de l'input
 });

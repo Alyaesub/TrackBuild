56594588de10133a2c3d47740301bf9f9664a7c6
Alyaesub <dadz13104@gmail.com>
2025-05-05 11:58:10 -0700

    add function delet and modify

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ const tableProject = document.getElementById("projectTableBody");
 //fonction qui ajoute un nouveau projet dans le tableau en dessous le l'input
 buttonAddProject.addEventListener("click", () => {
 	const projectName = inputNewProject.value.trim();
-	projects.push({ name: projectName, sessions: [] });
-	saveProjects(projectName);
 	if (projectName === "") {
+		alert("rentrez un nom de projet");
 		return;
 	}
+	projects.push({ name: projectName, sessions: [] });
+	saveProjects();
+
 	const newRow = document.createElement("tr"); //constante qui me crée un nouvelle ligne dans le tableau
 	const nameCell = document.createElement("td"); //pareil mais pour une cellule ou il y aura le titre du projet
 	nameCell.textContent = projectName; // la cellul en question
@@ -50,8 +52,47 @@ function loadProjects() {
 		<button class="button btn-danger btn-sm">Supprimer</button>
 		`;
 		newRow.appendChild(actionCell);
+
+		//metttre les ecouteur ici pour chopper les boutton créé avec innerHtml
+		//ecouteur pou rsupprimer le projet selectionner
+		const deleteBtn = actionCell.querySelector(".btn-danger");
+		const projectName = nameCell.textContent;
+		deleteBtn.addEventListener("click", () => {
+			deleteProject(projectName);
+		});
+
+		//ecouteur pour modofier le nom d'un projet
+		const modifyBtn = actionCell.querySelector(".btn-warning");
+		modifyBtn.addEventListener("click", () => {
+			const projectName = nameCell.textContent;
+			modifyProject(projectName);
+		});
 	});
 }
 loadProjects();
 
 //fonction pour supprimer un projet du localstorage
+function deleteProject(projectName) {
+	// Supprimer le projet du tableau
+	projects = projects.filter((p) => p.name !== projectName);
+	// utilise saveProject pour sauvegarder la nouvelle liste
+	saveProjects();
+	// Vider le tableau HTML
+	tableProject.innerHTML = "";
+	// j'utilise loadProject pour recharger depuis LocalStorage
+	loadProjects();
+}
+
+//function qui permet de modifier le nom d'un projet
+function modifyProject(projectName) {
+	const newName = prompt("Nouveau nom du projet :", projectName);
+	if (!newName || newName.trim() === "") return; // si annule ou vide
+
+	const project = projects.find((p) => p.name === projectName);
+	if (project) {
+		project.name = newName.trim();
+		saveProjects();
+		tableProject.innerHTML = "";
+		loadProjects();
+	}
+}
